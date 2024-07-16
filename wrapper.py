5cf4eae7f94b35ba6312ef703cd82ff2e83b47e2
Avf <Abdurahmon942120510@gmail.com>
2024-07-17 00:37:10 +0500

    from copy import copy,deepcopy original_list=[1,2,[4,5],[56,45,8]] shallow_copy_list=copy(original_list) print('shallow copy list:'shallow_copy_list) print('original list:',original list)

---
+++ wrapper.py
@@ -0,0 +1,21 @@
+
+# def polindrime(word):
+#     if  word==word[::-1]:
+#         return True      
+# print(polindrime('1221'))    
+
+
+# def is_polindrome(number:int):
+#     num=0
+#     if number<0:
+#         return False
+#     while number>0: #121
+#         num=number%10+num*10
+#         number=number//10
+#     if num==number:
+#         return True
+#     return False
+
+# print(is_polindrome(1212))
+
+
